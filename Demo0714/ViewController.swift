4fc6b7b2b5e36b86c0737b83b8fe69d009669a4c
LinJY0424 <f0983344166@gmail.com>
2020-07-14 19:05:22 +0800

    Initial Commit

---
+++ Demo0714/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo0714
+//
+//  Created by Duc-Thang Vo on 2020/7/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
